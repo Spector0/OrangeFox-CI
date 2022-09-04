42c373ed7426aabed5aaae4768b7a480b5c85a1d
Spector <33796688+Spector0@users.noreply.github.com>
2022-09-04 20:51:31 +0530

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,4 +2,4 @@ mkdir work
 cd work
 repo init -u $MANIFEST --depth=1 --partial-clone --clone-filter=blob:limit=10M --groups=all,-notdefault,-device,-darwin,-x86,-mips
 repo sync -j4
-git clone $DT_LINK --depth=1 --single-branch $DT_PATH
+git clone $DT_LINK --depth=1 --single-branch device/oneplus/oscar
